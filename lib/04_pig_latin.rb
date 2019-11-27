e232cb10aa031bd97ad27b2dfed895dae8bb87b5
Anvar <anvararty@Anvars-work-MacBook-Pro.local>
2019-11-27 10:13:57 -0800

    00-03

---
+++ lib/04_pig_latin.rb
@@ -0,0 +1,16 @@
+def translate(arr)
+  f = arr[/(?<vowel>[aeiou])(?<non_vowel>[^aeiou])/, "vowel"]
+if f == arr[0,1]
+  arr.concat("ay")
+arr
+else
+  first = arr[0,1]
+  arr[0,1] = ""
+  arr += first
+  arr.concat("ay")
+arr
+end
+if con = arr[/(?<vowel>[aeiou])(?<non_vowel>[^aeiou])/, "consonant"]
+
+end
+end
